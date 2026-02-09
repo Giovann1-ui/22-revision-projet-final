822e162772bd7aa71f3027a8a839f97e81e61544
Andriantsoa94 <voaharyralambo@gmail.com>
2026-02-09 21:38:57 +0300

    #

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -15,18 +15,15 @@ class UserController
         $user = $usermodel->authenticate($nom, $motDePasse);
 
         if ($user) {
-            // Authentification réussie
             session_start();
-            $_SESSION['user_id'] = $user['id']; // Si vous avez un champ id
+            $_SESSION['user_id'] = $user['id']; 
             $_SESSION['nom'] = $user['nom'];
 
-            // Récupérer le type si nécessaire
             $userType = $usermodel->getType($nom);
             $_SESSION['user_type'] = $userType;
 
             Flight::redirect('/dashboard');
         } else {
-            // Échec de l'authentification
             Flight::render('login', ['error' => 'Nom ou mot de passe incorrect']);
         }
     }
